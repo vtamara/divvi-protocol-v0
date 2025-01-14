086507cd9d02d212efa1132614c15e7dd1a846a5
Joseph Bergeron <jophish126@gmail.com>
2025-01-14 12:20:13 -0500

    Fix

--- scripts/calculateRevenue/protocols/beefy.ts
+++ scripts/calculateRevenue/protocols/beefy.ts
@@ -1,9 +1,9 @@
 import { RevenueResult } from '../../types'
 
-export async function calculateRevenue(
-  _address: string,
-  _startTimestamp: Date,
-  _endTimestamp: Date,
-): Promise<RevenueResult> {
+export async function calculateRevenue(_params: {
+  address: string
+  startTimestamp: Date
+  endTimestamp: Date
+}): Promise<RevenueResult> {
   return {}
 }
